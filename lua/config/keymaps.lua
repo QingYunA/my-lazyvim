85639a62cd041a6d2a7faa5388d8b69bfd59dcfa
sorlia <serein7z@163.com>
2023-11-03 17:59:46 +0800

    my lazyvim config

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+-- local Util = require("lazyvim.util")
+
+-- DO NOT USE THIS IN YOU OWN CONFIG!!
+-- use `vim.keymap.set` instead
+local map = vim.keymap.set
+map({ "i" }, "jj", "<cmd>noh<cr><esc>", { desc = "Escape and clear hlsearch" })
